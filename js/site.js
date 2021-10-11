2a60984fd090c6c0d98ad6b9ee1b2cdc0462542e
Alex R Baker <52913502+alexbaker2002@users.noreply.github.com>
2021-10-11 11:42:14 -0500

    fucntion written

--- js/site.js
+++ js/site.js
@@ -17,7 +17,59 @@
 
 
 
-function aFunc(){
-    
-  
+function getvalues() {
+    let kValue = 0;
+    kValue = "17";
+    kValue = parseInt(kValue);
+
+
+
+    if (isNaN(kValue)) {
+        alert(" K is not a number!!")
+        return;
+    }
+    let result = checkArray(kValue);
+    displayResult(result);
+}
+
+
+function checkArray(kValue) {
+    const array = [10, 15, 3, 7];
+
+    let output = false;
+
+    for (let i = 0; i <= array.length - 2; i++) {
+        for (let j = i + 1; j <= array.length - 1; j++) {
+            let outerValue = array[i];
+            let innerValue = array[j];
+            if (outerValue + innerValue == kValue) {
+                output = true;
+                return output;
+            }
+        }
+    }
+
+}
+
+function checkArrayB(kValue) {
+    // using indexOf()
+    const array = [10, 15, 3, 7];
+    let output = false;
+    for (let i = 0; i < array.length - 2; i++) {
+        let checkValue = kValue - array[i];
+        if (array.indexOf(checkValue, i) >= 0) {
+            output = true;
+            return output;
+        }
+    }
+    return output;
+}
+
+
+function displayResult(result) {
+    if (result) {
+        output.innerHTML = "True";
+    } else {
+        output.innerHTML = "False";
+    }
 }
